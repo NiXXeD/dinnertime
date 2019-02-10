2ba5709fc1ecb5c40f6ead62d48676afaa76fdd3
Andy Christianson <nix@nixxed.com>
2019-02-10 17:31:15 -0600

    Updates

---
+++ precache-manifest.6b607801a3ae25d250cac001228a7773.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7337e417b1d92c64da0bf9bfe18fe85a",
+    "url": "/dinnertime/static/media/github.7337e417.svg"
+  },
+  {
+    "revision": "69834e7db36f4a30cd96",
+    "url": "/dinnertime/static/js/runtime~main.69834e7d.js"
+  },
+  {
+    "revision": "825db07d402402e27a61",
+    "url": "/dinnertime/static/js/main.825db07d.chunk.js"
+  },
+  {
+    "revision": "9b4b1df9d997ccd241a6",
+    "url": "/dinnertime/static/js/2.9b4b1df9.chunk.js"
+  },
+  {
+    "revision": "825db07d402402e27a61",
+    "url": "/dinnertime/static/css/main.e48761d8.chunk.css"
+  },
+  {
+    "revision": "430d9724818d6c7459013dc0c78a8f8b",
+    "url": "/dinnertime/index.html"
+  }
+];
